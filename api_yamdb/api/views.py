5ac27fb82fb498ccc77806d1fa7df24e54fb7f02
“Елена <borovina@bk.ru>
2022-07-08 08:42:36 +0300

    «test»

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,9 +10,9 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
+
 from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
-
 from .permissions import (IsAdmin, IsAdminModeratorOwnerOrReadOnly,
                           IsAdminOrReadOnly)
 from .serializers import (CategorySerializer, CommentSerializer,
